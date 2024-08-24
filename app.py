91b36ec6bdffe2803689cc1cc4654d963eba241e
unknown <amirgorani466@gmail.com>
2024-08-24 14:42:02 +0330

    user and admin added

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from blueprint.general import app as general
+from blueprint.user import app as user
+from blueprint.admin import app as admin
 app = Flask(__name__)
 app.register_blueprint(general)
+app.register_blueprint(user)
+app.register_blueprint(admin)
 
 
 
